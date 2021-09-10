0ecaeb50017c4862fe91e9a87da82d4b93632098
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-09-10 21:24:57 +0200

    Use PostgreSQL for persistence and spin up workers

--- main.tf
+++ main.tf
@@ -40,8 +40,8 @@ resource "hsdp_container_host_exec" "server" {
       postgres_username = module.postgres.credentials.username
       postgres_password = module.postgres.credentials.password
       postgres_hostname = module.postgres.credentials.hostname
-      redis_host        = cloudfoundry_service_key.superset_redis_key.credentials.host
-      redis_password    = cloudfoundry_service_key.superset_redis_key.credentials.password
+      redis_host        = module.redis.credentials.hostname
+      redis_password    = module.redis.credentials.password
       enable_fluentd    = var.hsdp_product_key == "" ? "false" : "true"
       log_driver        = var.hsdp_product_key == "" ? "local" : "fluentd"
       superset_image    = var.superset_image
@@ -52,20 +52,9 @@ resource "hsdp_container_host_exec" "server" {
   }
 
   file {
-    content = templatefile("${path.module}/scripts/bootstrap-fluent-bit.sh.tmpl", {
-      ingestor_host    = data.hsdp_config.logging.url
-      shared_key       = var.hsdp_shared_key
-      secret_key       = var.hsdp_secret_key
-      product_key      = var.hsdp_product_key
-      custom_field     = var.hsdp_custom_field
-      fluent_bit_image = var.fluent_bit_image
+    content = templatefile("${path.module}/templates/superset_config.py", {
+      postgres_uri = module.postgres.credentials.uri
     })
-    destination = "/home/${var.user}/bootstrap-fluent-bit.sh"
-    permissions = "0755"
-  }
-
-  file {
-    content = templatefile("${path.module}/templates/superset_config.py", {})
     destination = "/home/${var.user}/superset_config.py"
     permissions = "0644"
   }
@@ -81,6 +70,53 @@ resource "hsdp_container_host_exec" "server" {
     "docker exec superset bash -c 'flask fab create-db'",
     "docker cp /home/${var.user}/superset_config.py superset:/app/pythonpath",
     "docker exec superset bash -c 'superset init'",
-    "docker restart superset"
+    "docker exec superset bash -c 'superset db upgrade'",
+    "docker restart superset",
+  ]
+}
+
+resource "hsdp_container_host_exec" "worker" {
+  triggers = {
+    cluster_instance_ids = hsdp_container_host.superset.id
+  }
+
+  depends_on = [hsdp_container_host_exec.server]
+
+  host        = hsdp_container_host.superset.private_ip
+  user        = var.user
+  private_key = var.private_key
+
+  file {
+    content = templatefile("${path.module}/scripts/bootstrap-worker.sh.tmpl", {
+      postgres_username = module.postgres.credentials.username
+      postgres_password = module.postgres.credentials.password
+      postgres_hostname = module.postgres.credentials.hostname
+      redis_host        = module.redis.credentials.hostname
+      redis_password    = module.redis.credentials.password
+      enable_fluentd    = var.hsdp_product_key == "" ? "false" : "true"
+      log_driver        = var.hsdp_product_key == "" ? "local" : "fluentd"
+      superset_image    = var.superset_image
+      superset_id       = random_id.id.hex
+    })
+    destination = "/home/${var.user}/bootstrap-worker.sh"
+    permissions = "0755"
+  }
+
+  file {
+    content = templatefile("${path.module}/templates/superset_config.py", {
+      postgres_uri = module.postgres.credentials.uri
+    })
+    destination = "/home/${var.user}/superset_config.py"
+    permissions = "0644"
+  }
+
+  # Bootstrap script called with private_ip of each node in the cluster
+  commands = [
+    "/home/${var.user}/bootstrap-worker.sh",
+    "docker exec superset-worker bash -c 'pip install werkzeug==0.16.0'",
+    "docker exec superset-worker bash -c 'pip install sqlalchemy-redshift'",
+    "docker exec superset-worker bash -c 'pip install sqlalchemy-vertica-python'",
+    "docker cp /home/${var.user}/superset_config.py superset-worker:/app/pythonpath",
+    "docker restart superset-worker",
   ]
 }
--- redis.tf
+++ redis.tf
@@ -1,15 +1,4 @@
-resource "cloudfoundry_service_instance" "superset_redis" {
-  space        = data.cloudfoundry_space.space.id
-  name         = "tf-superset-redis-${random_id.id.hex}"
-  service_plan = data.cloudfoundry_service.redis.service_plans[var.redis_plan]
-}
-
-resource "cloudfoundry_service_key" "superset_redis_key" {
-  name             = "key"
-  service_instance = cloudfoundry_service_instance.superset_redis.id
-}
-
-module "redis-service" {
+module "redis" {
   source  = "philips-labs/redis-service/hsdp"
   version = "0.2.0"
 
